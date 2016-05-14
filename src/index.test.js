12f1b6afd3a5eeda636880660e6757b44f7c2082
Shivani R <shivanir@thoughtworks.com>
2016-05-14 23:53:38 +0530

    Created spec file for index.js

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var starWars = require('./index');
+
+describe('star-wars', function() {
+    it('should pass', function() {
+        expect(true).to.be.true;
+    });
+});
