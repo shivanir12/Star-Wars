aac78f225316da7f095de11fd3d68d53f7d1d16d
Shivani R <shivanir@thoughtworks.com>
2016-05-15 13:58:23 +0530

    Added tests for library functions

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,25 @@ var expect = require('chai').expect;
 var starWars = require('./index');
 
 describe('star-wars', function() {
-    it('should pass', function() {
-        expect(true).to.be.true;
+    describe('all', function() {
+        it('should be an array of strings', function() {
+            expect(starWars.all).to.satisfy(arrayOfStrings);
+            function arrayOfStrings(array) {
+                return array.every(function(item) {
+                    return typeof item === 'string';
+                });
+            }
+        });
+
+        it('should include starWar name "a" ', function() {
+            expect(starWars.all).to.include("a");
+        });
+    });
+
+    describe('random', function() {
+        it('should get random starwar name', function() {
+            var starWar = starWars.random();
+            expect(starWars.all).to.include(starWar);
+        });
     });
 });
